4f4264f5a64de70646ae944456ad656185b91a1e
asharma39 <70486111+asharma39@users.noreply.github.com>
2020-10-25 10:05:18 -0700

    Add files via upload

---
+++ butane.R
@@ -0,0 +1,11 @@
+> V=3
+> n=1.0
+> Vbar=V/n
+> T=370.
+> R=0.083145
+> P=R*T/Vbar
+> cat("Ideal gas law ",P,"\n")
+> a=16.44
+> b=0.07245
+> P=R*T/(Vbar-b)-a/(Vbar*(Vbar+b)+b*(Vbar-b))
+> cat("Peng-Robinson equation of state",P,"\n")
---
+++ secher.R
@@ -0,0 +1,43 @@
+#Regression model for (bwt~bpd)
+
+> secher<-read.table("secher.txt", header=T)
+> model<-lm(bwt~bpd, data=secher)
+> sum.model<-summary(model)
+> R2<-sum.model$r.squared
+> f<-sum.model$fstatistic
+> p.value<-pf(f[1],f[2],f[3],lower.tail=F)
+> output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
+> cat(output)
+> intercept<-model$coefficients[1]
+> slope<-model$coefficients[2]
+> output<-sprintf("slope=%f intercept=%f",slope, intercept)
+> cat(output)
+> png("bwt_bpd.png")
+> plot(bwt~bpd, data=secher)
+> abline(model)
+> dev.off()
+
+#Regression model for (bwt~ad)
+
+> secher<-read.table("secher.txt", header=T)
+> model<-lm(bwt~ad, data=secher)
+> sum.model<-summary(model)
+> R2<-sum.model$r.squared
+> f<-sum.model$fstatistic
+> p.value<-pf(f[1],f[2],f[3],lower.tail=F)
+> output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
+> cat(output)
+> intercept<-model$coefficients[1]
+> slope<-model$coefficients[2]
+> output<-sprintf("slope=%f intercept=%f",slope, intercept)
+> cat(output)
+> png("bwt_ad.png")
+> plot(bwt~ad, data=secher)
+> abline(model)
+> dev.off()
+
+#Multiple linear regression data for (bwt~bpd+ad)
+> secher<-read.table("secher.txt", header=T)
+> names(secher)
+> model<-lm(bwt~bpd+ad, data=secher)
+> summary(model)
